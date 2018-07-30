e068cdc032af04a9bb53f9675eef000bbad06dba
MDang92 <MDang92@users.noreply.github.com>
2018-07-30 17:20:36 +0200

    Update appveyor_install_cuda.cmd
    Changed from CUDA 8.0 to CUDA 9.2

--- scripts/appveyor/appveyor_install_cuda.cmd
+++ scripts/appveyor/appveyor_install_cuda.cmd
@@ -1,14 +1,14 @@
 @echo off
-echo Downloading CUDA toolkit 8 ...
-appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe -FileName cuda_8.0.44_windows.exe
-echo Installing CUDA toolkit 8 ...
-cuda_8.0.44_windows.exe -s compiler_8.0 ^
-                           cublas_8.0 ^
-                           cublas_dev_8.0 ^
-                           cudart_8.0 ^
-                           curand_8.0 ^
-                           curand_dev_8.0 ^
-                           nvml_dev_8.0
+echo Downloading CUDA toolkit 9.2 ...
+appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers/cuda_9.2.148_windows -FileName cuda_9.2.148_windows.exe
+echo Installing CUDA toolkit 9.2 ...
+cuda_9.2.148_windows.exe -s compiler_9.2 ^
+                           cublas_9.2 ^
+                           cublas_dev_9.2 ^
+                           cudart_9.2 ^
+                           curand_9.2 ^
+                           curand_dev_9.2 ^
+                           nvml_dev_9.2
 :: Add CUDA toolkit to PATH
-set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\libnvvp;%PATH%
+set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.2\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.2\libnvvp;%PATH%
 nvcc -V
